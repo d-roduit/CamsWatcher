d76c1031e440565e472cf206f30c62b169c6ff20
d-roduit <37080343+d-roduit@users.noreply.github.com>
2022-05-11 00:58:11 +0200

    Add map functionality

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,9 +1,326 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, Alert, Modal, Button, Platform } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { API_BASE_URL, API_KEY } from '@env';
+
+const mapStyle = [
+    {
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#242f3e"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#746855"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#242f3e"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#263c3f"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#6b9a76"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#38414e"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#212a37"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9ca5b3"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#746855"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#1f2835"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#f3d19c"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2f3948"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.station",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#17263c"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#515c6d"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#17263c"
+            }
+        ]
+    }
+];
 
 function MapScreen() {
+
+    const [loadingUserLocation, setLoadingUserLocation] = useState(false);
+    const [initialRegion, setInitialRegion] = useState({
+        latitude: 20.700689035749388,
+        latitudeDelta: 110.88454576378447,
+        longitude: 20.830085165798664,
+        longitudeDelta: 126.56248658895493
+    });
+    const [cameras, setCameras] = useState([]);
+
+    const getUserLocation = async () => {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            return null;
+        }
+
+        setLoadingUserLocation(true);
+        const userLocation = await Location.getCurrentPositionAsync({});
+        if (typeof userLocation === "undefined") {
+            Alert.alert('Location not found');
+            return null;
+        }
+        setLoadingUserLocation(false);
+
+        return userLocation;
+    }
+
+    const getCoordinatesForAPIFromLocation = ({ latitude, latitudeDelta, longitude, longitudeDelta }) => {
+        return {
+            topRight: {
+                latitude: latitude + (latitudeDelta / 2),
+                longitude: longitude + (longitudeDelta / 2)
+            },
+            bottomLeft: {
+                latitude: latitude - (latitudeDelta / 2),
+                longitude: longitude - (longitudeDelta / 2)
+            },
+            zoom: Math.round(Math.log(360 / longitudeDelta) / Math.LN2) + 2.5
+        }
+    }
+
+    const determineInitialAreaToDisplay = async () => {
+        let areaToDisplay = getCoordinatesForAPIFromLocation(initialRegion);
+
+        try {
+            const userLocation = await getUserLocation();
+            if (userLocation !== null) {
+                const userRegion = {
+                    latitude: userLocation.coords.latitude,
+                    latitudeDelta: 0.5,
+                    longitude: userLocation.coords.longitude,
+                    longitudeDelta: 0.5,
+                };
+                areaToDisplay = getCoordinatesForAPIFromLocation(userRegion);
+                setInitialRegion(userRegion);
+            }
+        } catch (err) {
+            console.err(err);
+        }
+
+        return areaToDisplay;
+    }
+
+    const fetchCamerasInArea = ({ topRight, bottomLeft, zoom }) => {
+        const getCamerasAPIEndpoint = `${API_BASE_URL}/map/${topRight.latitude},${topRight.longitude},${bottomLeft.latitude},${bottomLeft.longitude},${zoom}?show=webcams:image,location`;
+        fetch(getCamerasAPIEndpoint, { headers: { "x-windy-key": API_KEY } })
+            .then(response => response.json())
+            .then(data => setCameras(data.result.webcams))
+            .catch(err => console.error(err));
+    }
+
+    const handleOnRegionChangeComplete = region => {
+        fetchCamerasInArea(getCoordinatesForAPIFromLocation(region))
+    };
+
+    useEffect(() => {
+        (async () => {
+            const baseArea = await determineInitialAreaToDisplay();
+            fetchCamerasInArea(baseArea);
+        })();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <Text>MapScreen</Text>
+            <MapView
+                style={styles.mapview}
+                userInterfaceStyle={"dark"}
+                customMapStyle={mapStyle}
+                toolbarEnabled={false}
+                mapType={"standard"}
+                region={initialRegion === null ? null : {
+                    latitude: initialRegion.latitude,
+                    latitudeDelta: initialRegion.latitudeDelta,
+                    longitude: initialRegion.longitude,
+                    longitudeDelta: initialRegion.longitudeDelta,
+                }}
+                onTouchMove={() => setInitialRegion(null)}
+                onRegionChangeComplete={handleOnRegionChangeComplete}
+            >
+                {
+                    cameras.map((camera, index) => (
+                        <Marker
+                            key={index}
+                            coordinate={{
+                                latitude: camera.location.latitude,
+                                longitude: camera.location.longitude
+                            }}
+                            image={{ uri: camera.image.current.icon }}
+                        >
+                            <Callout
+                                tooltip
+                                onPress={e => {
+                                    if (
+                                        Platform.OS === 'ios' ||
+                                        e.nativeEvent.action === 'marker-inside-overlay-press' ||
+                                        e.nativeEvent.action === 'callout-inside-press'
+                                    ) {
+                                        return;
+                                    }
+
+                                    Alert.alert('callout pressed');
+                                }}
+                                style={styles.customView}
+                            >
+                                <View style={styles.calloutContainer}>
+                                    <View style={styles.bubble}>
+                                        <View style={styles.amount}>
+                                            <Text style={styles.calloutText}>{camera.title}</Text>
+                                                <Button onPress={() => { Alert.alert("button pressed")}} title="Open"></Button>
+                                        </View>
+                                    </View>
+                                    <View style={styles.arrowBorder} />
+                                    <View style={styles.arrow} />
+                                </View>
+                            </Callout>
+                        </Marker>
+                    ))
+                }
+            </MapView>
+
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={loadingUserLocation}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Fetching your location <ActivityIndicator /></Text>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 }
@@ -12,6 +329,68 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "black"
+    },
+    mapview: {
+        flex: 1
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        backgroundColor: "rgba(0, 0, 0, 0.9)",
+        borderRadius: 10,
+        padding: 15,
+        alignItems: "center",
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+    },
+    modalText: {
+        color: "white"
+    },
+    calloutContainer: {
+        flexDirection: 'column',
+        alignSelf: 'flex-start',
+    },
+    calloutText: {
+        textAlign: "center"
+    },
+    bubble: {
+        width: 140,
+        flexDirection: 'row',
+        alignSelf: 'flex-start',
+        backgroundColor: 'lightblue',
+        paddingHorizontal: 20,
+        paddingVertical: 12,
+        borderRadius: 6,
+        borderColor: 'lightblue',
+        borderWidth: 0.5,
+    },
+    amount: {
+        flex: 1,
+    },
+    arrow: {
+        backgroundColor: 'transparent',
+        borderWidth: 16,
+        borderColor: 'transparent',
+        borderTopColor: 'lightblue',
+        alignSelf: 'center',
+        marginTop: -32,
+    },
+    arrowBorder: {
+        backgroundColor: 'transparent',
+        borderWidth: 16,
+        borderColor: 'transparent',
+        borderTopColor: 'lightblue',
+        alignSelf: 'center',
+        marginTop: -0.5,
     },
 });
 
