74ef5495c27ca66a0caab322b3e5456b0c60f7ed
d-roduit <37080343+d-roduit@users.noreply.github.com>
2022-05-13 00:49:00 +0200

    Add camera stream in Webview

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,13 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function CameraScreen({ route, navigation }) {
+function CameraScreen({ route }) {
 
     const { camera } = route.params;
 
+    const getFirstAvailableStreamURL = () => {
+        const streams = ["live", "day", "month", "year", "lifetime"];
+
+        for (let stream of streams) {
+            if (camera?.player?.[stream]?.available) {
+                return camera.player[stream].embed || "";
+            }
+        }
+        return "";
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>{camera.title}</Text>
-        </View>
+        <WebView
+            style={styles.container}
+            source={{ uri: getFirstAvailableStreamURL() }}
+        />
     );
 }
 
